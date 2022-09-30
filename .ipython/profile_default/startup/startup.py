d0208187c8c7f9e9072d321a3342e38f3b4c9671
Lucas R <github@lucas.rs>
2022-09-30 19:29:59 -0300

    add ipython startup script

---
+++ .ipython/profile_default/startup/startup.py
@@ -0,0 +1,13 @@
+import datetime
+import json
+import math
+import operator
+import os
+import random
+import sys
+import time
+
+from collections import Counter
+from decimal import Decimal
+from functools import reduce
+
