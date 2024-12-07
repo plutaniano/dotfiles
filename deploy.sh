fd122321ea4ed6ecaedfa0374c5efdbe6c9f6854
Lucas Rodrigues <git@lucas.rs>
2024-12-07 01:09:42 -0300

    update

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+set -euo pipefail
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+/opt/homebrew/bin/brew update
+/opt/homebrew/bin/brew upgrade
+/opt/homebrew/bin/brew bundle install
+
+
+# scroll direction
+defaults write -g com.apple.swipescrolldirection -boolean NO
+
+# key repetition
+defaults write -g InitialKeyRepeat -int 12
+defaults write -g KeyRepeat -int 2
+
+# Dock autohide
+defaults write com.apple.dock autohide -bool true
+
